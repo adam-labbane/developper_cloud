8b03f6be275fadb213bccec6187dfd9217f8b96a
adam-labbane <adamlabbane.dev@gmail.com>
2026-04-28 13:41:32 +0200

    Add Firebase SDK, metro config and firebaseConfig

---
+++ firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJCCFLRkiiZaCjN0IiNrhqEc9xkNwzBck",
+  authDomain: "web-cloud-ynov.firebaseapp.com",
+  projectId: "web-cloud-ynov",
+  storageBucket: "web-cloud-ynov.appspot.com",
+  messagingSenderId: "864572196508",
+  appId: "1:864572196508:web:a958f5a1710aeaebc40f59",
+  measurementId: "G-QM5KYQF0N7"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
